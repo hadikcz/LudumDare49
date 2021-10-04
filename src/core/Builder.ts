38916c90560f328f5a4c6b0bb118bb9d3c41fb05
Hadik <hadikcze@gmail.com>
2021-10-04 15:06:50 +0200

    BLocking building everywhere

--- src/core/Builder.ts
+++ src/core/Builder.ts
@@ -1,12 +1,13 @@
 import WorldEnvironment from 'core/WorldEnvironment';
-import Splitter from 'entity/pipeSystem/Splitter';
-import { Building } from 'enums/Building';
-import { Depths } from 'enums/Depths';
-import GameScene from 'scenes/GameScene';
-import Image = Phaser.GameObjects.Image;
 import HeatingPlant from 'entity/HeatingPlant';
 import Combiner from 'entity/pipeSystem/Combiner';
+import Splitter from 'entity/pipeSystem/Splitter';
 import Switch from 'entity/pipeSystem/Switch';
+import { Building } from 'enums/Building';
+import { Depths } from 'enums/Depths';
+import TransformHelpers from 'helpers/TransformHelpers';
+import GameScene from 'scenes/GameScene';
+import Image = Phaser.GameObjects.Image;
 
 export default class Builder {
 
@@ -30,7 +31,16 @@ export default class Builder {
 
     update (): void {
         if (this.buildMode !== null) {
+            const size = this.getBuildingSize(this.buildMode);
+            const canPlace = this.canPlace(this.scene.input.activePointer.worldX, this.scene.input.activePointer.worldY, size);
             this.previewImage.setPosition(this.scene.input.activePointer.worldX, this.scene.input.activePointer.worldY).setVisible(true);
+            // this.previewImage.
+
+            if (canPlace) {
+                this.previewImage.setTint(0x00FF00);
+            } else {
+                this.previewImage.setTint(0xFF0000);
+            }
         }
     }
 
@@ -74,5 +84,89 @@ export default class Builder {
         this.previewImage.setPosition(-100, -100).setVisible(false);
         this.buildMode = null;
         this.scene.ui.hideBuildMode();
+    }
+
+    private canPlace (x, y, size): boolean {
+        let nearestValue = Infinity;
+
+        for (let object of this.worldEnvironment.heaterGroup.getChildren()) {
+            if (!object.active) continue;
+            // @ts-ignore
+            let distance = TransformHelpers.getDistanceBetween(x, y, object.x, object.y) - 30;
+            if (distance < nearestValue) {
+                nearestValue = distance;
+            }
+        }
+
+        for (let object of this.worldEnvironment.buildingsGroup.getChildren()) {
+            if (!object.active) continue;
+            // @ts-ignore
+            let distance = TransformHelpers.getDistanceBetween(x, y, object.x, object.y);
+            if (distance < nearestValue) {
+                nearestValue = distance;
+            }
+        }
+
+        for (let object of this.worldEnvironment.factoriesGroup.getChildren()) {
+            if (!object.active) continue;
+            // @ts-ignore
+            let distance = TransformHelpers.getDistanceBetween(x, y, object.x, object.y);
+            if (distance < nearestValue) {
+                nearestValue = distance;
+            }
+        }
+
+        for (let object of this.worldEnvironment.combiners.getChildren()) {
+            if (!object.active) continue;
+            // @ts-ignore
+            let distance = TransformHelpers.getDistanceBetween(x, y, object.x, object.y);
+            if (distance < nearestValue) {
+                nearestValue = distance;
+            }
+        }
+
+        for (let object of this.worldEnvironment.splitters.getChildren()) {
+            if (!object.active) continue;
+            // @ts-ignore
+            let distance = TransformHelpers.getDistanceBetween(x, y, object.x, object.y);
+            if (distance < nearestValue) {
+                nearestValue = distance;
+            }
+        }
+
+        for (let object of this.worldEnvironment.switches.getChildren()) {
+            if (!object.active) continue;
+            // @ts-ignore
+            let distance = TransformHelpers.getDistanceBetween(x, y, object.x, object.y);
+            if (distance < nearestValue) {
+                nearestValue = distance;
+            }
+        }
+        console.log(nearestValue);
+        return nearestValue > size; // because placing object and object around
+    }
+
+    private getBuildingSize (building: Building): number {
+        switch (building) {
+            case Building.HEATING_PLANT:
+                return 100;
+            case Building.COMBINER:
+            case Building.SWITCH:
+            case Building.SPLITTER:
+                return 32;
+            default:
+                return 32;
+        }
+    }
+
+    private getSizeOfBuildingByFrameName (frameName: string): number {
+        const frame = this.scene.textures.getFrame('assets', frameName);
+        if (!frame) {
+            console.error('frame not found ' + frameName);
+            return 32;
+        }
+        let size = (frame.width + frame.height) / 2;
+
+        return size;
     }
 }
